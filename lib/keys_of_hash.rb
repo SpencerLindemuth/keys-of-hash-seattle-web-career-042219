10ef2c063073c0e9438570abc71c6d5ea2065c80
Spencer Lindemuth <lindemut@gmail.com>
2019-04-03 16:23:32 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,8 +2,10 @@ require 'pry'
 class Hash
   def keys_of(*arguments)
     # code goes here
-    x = collect do |key, value|
-      arguments.include?(value)
+    array = []
+    each do |key, value|
+      if arguments.include?(value)
+        array << key
       binding.pry
     end
   end
